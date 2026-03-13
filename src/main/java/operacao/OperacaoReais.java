9537f4b6cd93ef632d69e7c6259995ca81e2c6f9
jotape <jotape@pop-os>
2026-03-13 10:03:22 -0300

    add OperacaoReais

---
+++ src/main/java/operacao/OperacaoReais.java
@@ -0,0 +1,46 @@
+package operacao;
+
+/**
+ * Implementação das operações matemáticas para números reais.
+ * Esta enumeração centraliza a lógica de cálculo, símbolos e elementos neutros.
+ * 
+ * @author joaopazvdo
+ */
+public enum OperacaoReais implements Operacao<Double> {
+	
+	MULTIPLICACAO("*", 1.0) {
+		/** {@inheritDoc} */
+		@Override
+		public Double faz(Double v1, Double v2) {
+			return v1 * v2;
+		}
+	},
+	SOMA("+", 0.0) {
+		/** {@inheritDoc} */
+		@Override
+		public Double faz(Double v1, Double v2) {
+			return v1 + v2;
+		}
+	};
+	
+	private String operador;
+	
+	private Double identidade;
+	
+	private OperacaoReais(String operador, Double identidade) {
+		this.operador = operador;
+		this.identidade = identidade;
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public boolean operadorEquals(String outroOperador) {
+		return operador.equals(outroOperador);
+	}
+	
+	/** {@inheritDoc} */
+	@Override
+	public Double getIdentidade() {
+		return identidade;
+	}
+}
