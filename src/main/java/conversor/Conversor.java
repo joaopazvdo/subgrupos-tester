fabd95102c164f83c6d6cd6c34cde82148fe4ad4
= <=>
2026-03-13 13:34:09 -0300

    Finalizando o Controller

--- src/main/java/conversor/Conversor.java
+++ src/main/java/conversor/Conversor.java
@@ -1,5 +1,99 @@
 package conversor;
 
+import java.util.*;
+import java.util.regex.*;
+
 public class Conversor {
 
+	public Conversor(){
+
+	}
+
+    /**
+     * Método responsavel por trnasformar uma String no formato:
+     * s = 
+     * "n1, n2, n3",
+     * "n4, n5, n6",
+     * "n7, n8, n9"
+     * em uma matriz de Double, que sera armazenada em um hashSet.
+     * */
+	public HashSet<Double[][]> paraConjuntoMatriz(String conjuto){
+		List<Double[]> linhasTemporarias = new ArrayList<>();
+
+        Pattern padrao = Pattern.compile("\\{([^}]+)\\}");
+        Matcher buscador = padrao.matcher(conjuto);
+
+        while (buscador.find()) {
+            String conteudoLinha = buscador.group(1);
+
+            String[] numerosEmTexto = conteudoLinha.split(",");
+            Double[] linhaAtual = new Double[numerosEmTexto.length];
+
+            for (int i = 0; i < numerosEmTexto.length; i++) {
+                linhaAtual[i] = Double.parseDouble(numerosEmTexto[i].trim());
+            }
+
+            linhasTemporarias.add(linhaAtual);
+        }
+
+        Double[][] matrizFinal = linhasTemporarias.toArray(new Double[0][0]);
+
+        HashSet<Double[][]> conjunto = new HashSet<>();
+
+        conjunto.add(matrizFinal);
+
+        return conjunto;
+	}
+
+    /**
+     * Metodo responsavel por pegar uma String no formato:
+     * s = "n1, n2, n3, n4, n5"
+     * e trasforma-la em um hashSet com esses números
+     * */
+    public HashSet<Double> paraConjuntoNumerico(String conjunto){
+        HashSet<Double> conj = new HashSet<>();
+
+        String[] numeros = conjunto.split(",");
+
+        for(String parte: numeros){
+            Double num = Double.valueOf(parte.trim());
+            conj.add(num);
+        }
+
+        return conj;
+    }
+
+
+    /**
+     * Metodo responsavel por pegar uma String no formato:
+     * s = "000 000 000"
+     * e retornar "000" "000" "000"
+     * */
+    public HashSet<String> paraConjuntoBinario(String conjunto){
+        HashSet<String> conj = new HashSet<>();
+
+        String[] numeros = conjunto.split(" ");
+
+        for(String parte: numeros){
+            String num = parte.trim();
+            conj.add(num);
+        }
+        return conj;
+    }
+
+    /**
+     * Metodo reponsavel por pegar um String no formato:
+     * z6 = "6"
+     * e retornar um hasSet de todos os numero de 0  a z6 - 1
+     * */
+    public HashSet<Integer> paraConjuntoMod(String conjunto){
+        int z = Integer.parseInt(conjunto.trim());
+        HashSet<Integer> conj = new HashSet<>();
+
+        for(int i = 0; i < z; i++){
+            conj.add(i);
+        }
+
+        return conj;
+    }
 }
--- src/main/java/subgrupostester/SubGruposTester.java
+++ src/main/java/subgrupostester/SubGruposTester.java
@@ -1,5 +1,62 @@
 package subgrupostester;
 
-public class SubGruposTester {
+import conversor.Conversor;
+import java.util.*;
 
+public class SubGruposTester {
+	private Conversor conversor = new Conversor();
+
+	public SubGruposTester(){
+	}
+
+	public boolean ehSubGrupoNumerico(String conjuntoOriginal, String operacao, 
+		String subConjunto){
+
+		HashSet<Double> conjunto = conversor.paraConjuntoNumerico(conjuntoOriginal);
+		HashSet<Double> subConj = conversor.paraConjuntoNumerico(subConjunto);
+		Operacao op = OperacaoController.getOperacaoNumerica(operacao);
+
+		SubGrupo SubGrupo = new SubGrupo(conjunto, op, subConj);
+
+		return subGrupo.ehSubGrupo();
+	}
+
+	public boolean ehSubGrupoMatriz(String conjuntoOriginal, String operacao,
+	 String subConjunto){
+
+		HashSet<Double[][]> conjunto = conversor.paraConjuntoMatriz(conjuntoOriginal);
+		HashSet<Double[][]> subConj = conversor.paraConjuntoMatriz(subConjunto);
+		Operacao op = OperacaoController.getOperacaoMatriz(operacao);
+
+		SubGrupo SubGrupo = new SubGrupo(conjunto, op, subConj);
+
+		return subGrupo.ehSubGrupo();
+
+	}
+
+
+	public boolean ehSubGrupoBinario(String conjuntoOriginal, String operacao,
+	 String subConjunto){
+
+	 	HashSet<String> conjunto = conversor.paraConjuntoBinario(conjuntoOriginal);
+		HashSet<String> subConj = conversor.paraConjuntoBinario(subConjunto);
+		Operacao op = OperacaoController.getOperacaoBinaria(operacao);
+
+		SubGrupo SubGrupo = new SubGrupo(conjunto, op, subConj);
+
+		return subGrupo.ehSubGrupo();
+
+	}
+
+	public boolean ehSubGrupoMod(String conjuntoOriginal, String operacao,
+	 String subConjunto){
+
+	 	HashSet<Integer> conjunto = conversor.paraConjuntoMod(conjuntoOriginal);
+		HashSet<Integer> subConj = conversor.paraConjuntoMod(subConjunto);
+		Operacao op = OperacaoController.getOperacaoClasseCongruencia(operacao);
+
+		SubGrupo SubGrupo = new SubGrupo(conjunto, op, subConj);
+
+		return subGrupo.ehSubGrupo();
+	}
 }
