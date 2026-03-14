fbef4993216724c81c7799f6577b24c503567841
jotape <jotape@pop-os>
2026-03-14 12:29:56 -0300

    documentação

--- src/main/java/operacao/OperacaoMod.java
+++ src/main/java/operacao/OperacaoMod.java
@@ -1,9 +1,21 @@
 package operacao;
 
+/**
+ * Implementação das operações matemáticas para classes de congruência.
+ * Esta enumeração centraliza a lógica de cálculo, símbolos e elementos neutros.
+ * 
+ * @author joaopazvdo
+ */
 public enum OperacaoMod implements Operacao<String> {
 
 	MULTIPLICACAO("*", 1) {
 	
+		/**
+		* Realiza a multiplicação de duas classes de congruência.
+		* @param v1 Primeiro operando.
+		* @param v2 Segundo operando.
+	 	* @return O produto da multiplicação.
+		*/
 		@Override
 		public String faz(String v1, String v2) {
 			String[] operando1_mod1 = v1.split(" mod ");
@@ -12,7 +24,9 @@ public enum OperacaoMod implements Operacao<String> {
 			
 			String[] operando2_mod2 = v2.split(" mod ");
 			int operando2 = Integer.parseInt(operando2_mod2[0]);
-//			int mod2 = Integer.parseInt(operando2_mod2[1]);
+			int mod2 = Integer.parseInt(operando2_mod2[1]);
+			
+			verificaModsDiferentes(mod1, mod2);
 			
 			int resultado = (operando1 * operando2) % mod1;
 			return resultado + " mod " + mod1;
@@ -21,6 +35,12 @@ public enum OperacaoMod implements Operacao<String> {
 	
 	SOMA("+", 0) {
 		
+		/**
+		* Realiza a soma de duas classes de congruência.
+		* @param v1 Primeiro operando.
+		* @param v2 Segundo operando.
+	 	* @return O resultado da operação.
+		*/
 		@Override
 		public String faz(String v1, String v2) {
 			String[] operando1_mod1 = v1.split(" mod ");
@@ -29,7 +49,9 @@ public enum OperacaoMod implements Operacao<String> {
 			
 			String[] operando2_mod2 = v2.split(" mod ");
 			int operando2 = Integer.parseInt(operando2_mod2[0]);
-//			int mod2 = Integer.parseInt(operando2_mod2[1]);
+			int mod2 = Integer.parseInt(operando2_mod2[1]);
+			
+			verificaModsDiferentes(mod1, mod2);
 			
 			int resultado = (operando1 + operando2) % mod1;
 			return resultado + " mod " + mod1;
@@ -45,13 +67,33 @@ public enum OperacaoMod implements Operacao<String> {
 		this.identidade = identidade;
 	}
 	
+	/**
+	 * O elemento identidade ou neutro.
+	 * 
+	 * @param v um elemento de comparação para saber o módulo.
+	 * @return O elemento identidade ou neutro.
+	 */
 	@Override
 	public String getIdentidade(String v) {
-		return identidade + " mod 6";
+		String[] operando_mod = v.split(" mod ");
+		int mod = Integer.parseInt(operando_mod[1]);
+		return identidade + " mod " + mod;
 	}
 	
+	/** {@inheritDoc} */
 	@Override
 	public boolean operadorEquals(String outroOperador) {
 		return this.operador.equals(outroOperador);
+	}
+	
+	/**
+	 * @param mod1
+	 * @param mod2
+	 * @throws IllegalArgumentException Se os módulos forem diferentes
+	 */
+	public void verificaModsDiferentes(int mod1, int mod2) {
+		if (mod1 != mod2) {
+			throw new IllegalArgumentException("Operações com mods diferentes inválidas");
+		}
 	}
 }
