9a7eb8a4e74f727bb5e8f45f2d3a37ddb498f506
Jose Kauan Pereira <jose.kauan.pereira00@gmail.com>
2026-03-14 17:58:31 -0300

    Concertando o conversor

--- src/main/java/conversor/Conversor.java
+++ src/main/java/conversor/Conversor.java
@@ -1,16 +1,14 @@
 package conversor;
 
 import java.util.List;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import java.util.HashSet;
 
 public class Conversor {
-
-	public Conversor(){
-
-	}
 
     /**
      * Método responsavel por trnasformar uma String no formato:
@@ -20,7 +18,7 @@ public class Conversor {
      * "n7, n8, n9"
      * em uma matriz de Double, que sera armazenada em um hashSet.
      * */
-	public HashSet<Double[][]> paraConjuntoMatriz(String conjuto){
+	public static HashSet<Double[][]> paraConjuntoMatriz(String conjuto){
 		List<Double[]> linhasTemporarias = new ArrayList<>();
 
         Pattern padrao = Pattern.compile("\\{([^}]+)\\}");
@@ -53,7 +51,7 @@ public class Conversor {
      * s = "n1, n2, n3, n4, n5"
      * e trasforma-la em um hashSet com esses números
      * */
-    public HashSet<Double> paraConjuntoNumerico(String conjunto){
+    public static HashSet<Double> paraConjuntoNumerico(String conjunto){
         HashSet<Double> conj = new HashSet<>();
 
         String[] numeros = conjunto.split(",");
@@ -70,15 +68,15 @@ public class Conversor {
     /**
      * Metodo responsavel por pegar uma String no formato:
      * s = "000 000 000"
-     * e retornar "000" "000" "000"
+     * e retornar [000, 000, 000]
      * */
-    public HashSet<String> paraConjuntoBinario(String conjunto){
-        HashSet<String> conj = new HashSet<>();
+    public static HashSet<Integer> paraConjuntoBinario(String conjunto){
+        HashSet<Integer> conj = new HashSet<>();
 
         String[] numeros = conjunto.split(" ");
 
         for(String parte: numeros){
-            String num = parte.trim();
+            Integer num = Integer.parseInt(parte.trim(), 2);
             conj.add(num);
         }
         return conj;
@@ -89,12 +87,15 @@ public class Conversor {
      * z6 = "6"
      * e retornar um hasSet de todos os numero de 0  a z6 - 1
      * */
-    public HashSet<Integer> paraConjuntoMod(String conjunto){
-        int z = Integer.parseInt(conjunto.trim());
-        HashSet<Integer> conj = new HashSet<>();
+    public static HashSet<String> paraConjuntoMod(String conjunto){
+        HashSet<String> conj = new HashSet<>();
 
-        for(int i = 0; i < z; i++){
-            conj.add(i);
+        int[] numeros = Arrays.stream(conjunto.split("\\D+")).filter(s -> !s.isEmpty()).mapToInt(Integer::parseInt).toArray();
+        for(int i = 0; i < numeros.length; i++){
+            int num = numeros[i];
+            String mod = String.format("%d mod %d", num, numeros.length);
+
+            conj.add(mod);
         }
 
         return conj;
